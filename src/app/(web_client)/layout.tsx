192349de32f3916659372ae7623a4b4175793181
DESKTOP-FI6B674\admin <dangquocnhat1998@gmail.com>
2025-01-19 12:09:05 +0700

    add footer

--- src/app/(web_client)/layout.tsx
+++ src/app/(web_client)/layout.tsx
@@ -1,13 +1,12 @@
+import Container from '@/components/layout/Container';
+import Footer from '@/components/layout/Footer';
 import React, { PropsWithChildren } from 'react';
 
 export default function LayoutWebClient({ children }: PropsWithChildren) {
   return (
-    <div className="h-full w-full block relative">
-      {React.Children.map(children, (child) => {
-        if (child) {
-          return child;
-        }
-      })}
+    <div className="h-full w-full flex flex-col relative dark:bg-white dark:text-gray-700">
+      <Container>{children}</Container>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { cn } from '@/utils/tailwind';
+import React, { PropsWithChildren } from 'react';
+
+export default function Container({
+  children,
+  className,
+}: PropsWithChildren<{ className?: string }>) {
+  return (
+    <div className={cn('w-full h-full px-[360px]', className)}>{children}</div>
+  );
+}
