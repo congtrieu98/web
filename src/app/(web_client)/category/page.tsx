fa007b500f70b5242b10b8bf5ab19b1f3fbdb7b6
congtrieu98 <trieunguyen2806@gmail.com>
2025-05-05 21:49:57 +0700

    fix

--- src/app/(web_client)/category/page.tsx
+++ src/app/(web_client)/category/page.tsx
@@ -1,11 +1,5 @@
 
 import React from "react";
-
-export default function HomePage({ children }: { children: React.ReactNode }) {
-    return (
-        <div>
-            {children}
-        </div>
-
-    );
+export default function HomePage() {
+    return (<div></div>);
 }
--- src/app/(web_client)/products/page.tsx
+++ src/app/(web_client)/products/page.tsx
@@ -1,11 +1,6 @@
 
 import React from "react";
 
-export default function HomePage({ children }: { children: React.ReactNode }) {
-    return (
-        <div>
-            {children}
-        </div>
-
-    );
+export default function HomePage() {
+    return (<div></div>);
 }
